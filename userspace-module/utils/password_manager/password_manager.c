3a905937cbdf689c36844a76b9f0bbd727ff6e59
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2023-11-27 17:55:31 +0100

    small changes to password managing
    All of this is commented at the moment. These methods are useless for now

--- userspace-module/utils/password_manager/password_manager.c
+++ userspace-module/utils/password_manager/password_manager.c
@@ -1,10 +1,24 @@
 //TODO: This util will handle requesting keys to kernel
 
-#include <stddef.h>
+#include "password_manager.h"
+#include "../crypt-utils/crypt-utils.h"
+/*
+char *true_key;
 
-char *request_key(){
+int insert_key(char* key, char* cert, int is_sys_call)
+{
+    if (is_sys_call == WITH_SYS_CALL)
+    {
+        fprintf(stderr, "The kernal module has not been implemented yet, saving key in userspace\n \
+                        This will change in the future");
+        insert_key(key, cert, WITHOUT_SYS_CALL);
+    }
+    return rebuild_key(key, cert, true_key);
+}
+
+char *request_key(int is_sys_call){
     return NULL;
 }
-int delete_key(){
+int delete_key(int is_sys_call){
     return -1;
-}
+}*/
--- userspace-module/utils/password_manager/password_manager.h
+++ userspace-module/utils/password_manager/password_manager.h
@@ -1,2 +1,9 @@
-char *request_key();
-int delete_key();
+#include <stddef.h>
+#include <stdio.h>
+
+#define WITH_SYS_CALL 1
+#define WITHOUT_SYS_CALL 0
+/*
+int insert_key(char* key, char* cert, int is_sys_call);
+char *request_key(int is_sys_call);
+int delete_key(int is_sys_call);*/
